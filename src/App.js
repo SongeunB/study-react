cd0bfd412e8edda0f8eba1a1f1abd2986c5df3c8
songeun <songeun.bb@gmail.com>
2025-12-21 16:20:56 +0900

    LifeCycle

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
 import React, { Component } from 'react';
-import IterationSample from './IterationSample';
+import LifeCycleSmple from './LifeCycleSmple';
+import ErrorBoundary from './ErrorBoundary';
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: '#000000',
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
   render() {
     return (
       <div>
-        <IterationSample />
+        <ErrorBoundary>
+          <LifeCycleSmple color={this.state.color} />
+        </ErrorBoundary>
+        <button onClick={this.handleClick}>랜덤 색상</button>
       </div>
     );
   }
