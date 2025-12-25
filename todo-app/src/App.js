0a7ba0a8495f648bd422d93381b94b788a2eb862
songeun <songeun.bb@gmail.com>
2025-12-25 20:32:16 +0900

    todo-list init

---
+++ todo-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TodoTemplate from "./components/TodoTemplate";
+
+const App = () => {
+  return (
+    <div>
+      <TodoTemplate>Todo App 만들기</TodoTemplate>
+    </div>
+  );
+};
+
+export default App;
